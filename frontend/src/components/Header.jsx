4dd8243fb7859adff8e06a5037c881e0a8bd324f
Afaaf Hariri <haririafaaf@gmail.com>
2024-10-04 21:32:03 +0530

    Fixed FAQ white screen issue

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -9,23 +9,32 @@ const Header = () => {
                     <Link to="/" className="hover:text-gray-300">
                         Home
                     </Link>
-                    <Link to="/productViews" className="hover:text-gray-300">Products</Link>
+                    <Link to="/productViews" className="hover:text-gray-300">
+                        Products
+                    </Link>
                     <Link to="/eventViews" className="hover:text-gray-300">
                         Events
-                    </Link>
-                    <Link to="/donations/herosection" className="hover:text-gray-300">Donation</Link>
-                    <Link to="/userRefunds" className="hover:text-gray-300">Refund</Link>
-                    <Link to="/return" className="hover:text-gray-300">
-                        Return
-                    </Link>
-                    <Link to="/createtickets" className="hover:text-gray-300">
-                        HelpDesk
                     </Link>
                     <Link
                         to="/ViewAvailablePacks/viewpacks"
                         className="hover:text-gray-300"
                     >
                         Premium Membership
+                    </Link>
+                    <Link
+                        to="/donations/herosection"
+                        className="hover:text-gray-300"
+                    >
+                        Donation
+                    </Link>
+                    <Link to="/userRefunds" className="hover:text-gray-300">
+                        Refund
+                    </Link>
+                    <Link to="/return" className="hover:text-gray-300">
+                        Return
+                    </Link>
+                    <Link to="/createtickets" className="hover:text-gray-300">
+                        HelpDesk
                     </Link>
                 </div>
             </nav>
