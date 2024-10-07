48abaf74c5c986157801ff64262a78110d339076
Afaaf Hariri <haririafaaf@gmail.com>
2024-10-07 15:02:53 +0530

    nav order

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,18 +5,27 @@ const Header = () => {
   return (
     <header className="bg-gray-800 text-white p-4">
       <nav className="container mx-auto flex justify-around">
-        <div className="flex space-x-7">
+        <div className="flex space-x-11">
           <Link to="/" className="hover:text-gray-300">
             Home
           </Link>
           <Link to="/eventViews" className="hover:text-gray-300">
             Events
           </Link>
-          <Link to="/productViews" className="hover:text-gray-300">
-            Products
+          <Link to="/" className="hover:text-gray-300">
+            Rescue
           </Link>
           <Link to="/donations/herosection" className="hover:text-gray-300">
             Donation
+          </Link>
+          <Link
+            to="/ViewAvailablePacks/viewpacks"
+            className="hover:text-gray-300"
+          >
+            Memberships
+          </Link>
+          <Link to="/productViews" className="hover:text-gray-300">
+            Products
           </Link>
           <Link to="/userRefunds" className="hover:text-gray-300">
             Refund
@@ -24,17 +33,8 @@ const Header = () => {
           <Link to="/returns/create" className="hover:text-gray-300">
             Return
           </Link>
-          <Link
-            to="/ViewAvailablePacks/viewpacks"
-            className="hover:text-gray-300"
-          >
-            Premium Membership
-          </Link>
           <Link to="/createtickets" className="hover:text-gray-300">
-            HelpDesk
-          </Link>
-          <Link to="/" className="hover:text-gray-300">
-            RescueTurtle
+            Help
           </Link>
         </div>
       </nav>
